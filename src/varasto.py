aa33e580e77c324f703a33de6bdfc6b5e55bd597
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-11-07 15:45:31 +0200

    Riko pylint

--- src/varasto.py
+++ src/varasto.py
@@ -18,6 +18,7 @@ class Varasto:
 
     # huom: ominaisuus voidaan myös laskea.
     # Ei tarvita erillistä kenttää viela_tilaa tms.
+    # aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
